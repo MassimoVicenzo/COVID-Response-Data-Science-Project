07f71e2a594f01be97345fc2b2051e2871badcdc
Massimo Vicenzo <vicenzomassimo@gmail.com>
2021-11-25 22:28:05 -0500

    First iteration of the code to gather our data

---
+++ src/gathering_tweets.py
@@ -0,0 +1,74 @@
+import tweepy as tw
+import os.path as op
+import argparse as ap
+import csv
+
+#Change this list if you want to look for different words
+KEYWORDS = ["vaccine","vaccination","COVID","pandemic","outbreak","virus","Pfizer","BioNTech","Moderna"]
+
+def parse_input():
+    parser = ap.ArgumentParser()
+    parser.add_argument('-b', '--bearer')
+    parser.add_argument('-o', '--output')
+
+    args = parser.parse_args()
+
+    script_dirname = op.dirname(__file__)
+    output_path = op.normpath(op.join(script_dirname, args.output))
+
+    return [args.bearer, output_path]
+
+def setup_auth(bearer):
+    
+    print("Setting up authentication")
+
+    client = tw.Client(bearer_token=bearer, wait_on_rate_limit=True)
+    
+    print("Authentication finished")
+
+    return client
+
+def collect_tweets(api):
+
+    print("Collecting tweets")
+
+    #Using the keywords array, we search for them in english non-retweet tweets
+    query = " lang:en -is:retweet -is:reply"
+    query = '(' + ' OR '.join(KEYWORDS) + ')' + query #LIMIT OF 512 CHARACTERS FOR THE BASIC ACCOUNT
+    
+    #Currently we are grabbing the id, text, and metrics like replies and likes
+    tweets = api.search_recent_tweets(query=query, max_results=100,tweet_fields=["id","text","public_metrics"]) #tweet.fields=organic_metrics
+    
+    print("Finished collecting tweets")
+
+    return tweets
+
+def extract_and_format(tweets, out_path):
+    
+    print("Extracting pertinant information from tweets")
+    
+    #Here we grab the information we need from the tweets we scraped
+    with open(out_path,'w', newline='',encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(['id','text','retweet_count','reply_count','like_count'])
+        for tweet in tweets.data:
+            arr = [
+                tweet.id,tweet.text,
+                tweet.public_metrics['retweet_count'],
+                tweet.public_metrics['reply_count'],
+                tweet.public_metrics['like_count']
+                ]
+            writer.writerow(arr)
+
+    print("Outputed CSV file")
+
+
+def main():
+    [bearer, out_path] = parse_input()
+    api = setup_auth(bearer)
+    tweets = collect_tweets(api) 
+    extract_and_format(tweets,out_path)
+
+
+if __name__ == '__main__':
+    main()
